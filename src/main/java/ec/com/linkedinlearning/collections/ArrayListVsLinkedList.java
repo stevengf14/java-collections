8ba286e934d8bbda6d3f1fb1af3b3e5cf636e6cc
Steven <stevengf14@gmail.com>
2024-05-10 21:33:09 -0500

    ArrayList vs LinkedList (add)

--- src/main/java/ec/com/linkedinlearning/collections/ArrayListVsLinkedList.java
+++ src/main/java/ec/com/linkedinlearning/collections/ArrayListVsLinkedList.java
@@ -22,11 +22,18 @@ public class ArrayListVsLinkedList {
             arrayList.add(num);
             linkedList.add(num);
         }
-        getCompare(arrayList, linkedList, 0);
-        getCompare(arrayList, linkedList, LIM / 4);
-        getCompare(arrayList, linkedList, LIM / 2);
-        getCompare(arrayList, linkedList, 3 * LIM / 4);
-        getCompare(arrayList, linkedList, LIM - 1);
+//        getCompare(arrayList, linkedList, 0);
+//        getCompare(arrayList, linkedList, LIM / 4);
+//        getCompare(arrayList, linkedList, LIM / 2);
+//        getCompare(arrayList, linkedList, 3 * LIM / 4);
+//        getCompare(arrayList, linkedList, LIM - 1);
+
+        addCompare(arrayList, linkedList, -1);
+        addCompare(arrayList, linkedList, 0, -1);
+        addCompare(arrayList, linkedList, LIM / 4, -3);
+        addCompare(arrayList, linkedList, LIM / 2, -4);
+        addCompare(arrayList, linkedList, 3 * LIM / 4, -5);
+        addCompare(arrayList, linkedList, LIM - 1, -6);
     }
 
     public static void getCompare(List<Integer> arrayList, List<Integer> linkedList, int position) {
@@ -41,6 +48,38 @@ public class ArrayListVsLinkedList {
         linkedList.get(position);
         long milisD = System.currentTimeMillis();
         System.out.println("linkedList.get(" + position + "): " + (milisD - milisC));
+    }
+
+    public static void addCompare(List<Integer> arrayList, List<Integer> linkedList, int value) {
+        System.out.println("Add Compare");
+
+        long milisA = System.currentTimeMillis();
+        arrayList.add(value);
+        long milisB = System.currentTimeMillis();
+        System.out.println("arrayList.add(): " + (milisB - milisA));
+
+        long milisC = System.currentTimeMillis();
+        linkedList.add(value);
+        long milisD = System.currentTimeMillis();
+        System.out.println("linkedList.add(): " + (milisD - milisC));
+
+        System.out.println();
+    }
+
+    public static void addCompare(List<Integer> arrayList, List<Integer> linkedList, int position, int value) {
+        System.out.println("Add Compare, position: " + position);
+
+        long milisA = System.currentTimeMillis();
+        arrayList.add(position, value);
+        long milisB = System.currentTimeMillis();
+        System.out.println("arrayList.add(" + position + "): " + (milisB - milisA));
+
+        long milisC = System.currentTimeMillis();
+        linkedList.add(position, value);
+        long milisD = System.currentTimeMillis();
+        System.out.println("linkedList.add(" + position + "): " + (milisD - milisC));
+
+        System.out.println();
     }
 
 }
