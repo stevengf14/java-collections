222190aacafc87337d5b420dc98d2789142f636c
Steven <stevengf14@gmail.com>
2024-05-15 22:50:02 -0500

    StackClass

---
+++ src/main/java/ec/com/linkedinlearning/collections/StackClass.java
@@ -0,0 +1,34 @@
+package ec.com.linkedinlearning.collections;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Steve
+ */
+public class StackClass {
+
+    public static void main(String[] args) {
+        Stack<Integer> pila = new Stack<>();
+        pila.push(1);
+        pila.push(2);
+        pila.push(3);
+        System.out.println(pila);
+        System.out.println("peek: " + pila.peek());
+        System.out.println(pila);
+        System.out.println("peek: " + pila.peek());
+        System.out.println(pila);
+        System.out.println("pop : " + pila.pop());
+        System.out.println(pila);
+        System.out.println("pop : " + pila.pop());
+        System.out.println(pila);
+        pila.push(4);
+        System.out.println(pila);
+        System.out.println("pop : " + pila.pop());
+        System.out.println(pila);
+        System.out.println("pop : " + pila.pop());
+        System.out.println("pila.empty()? " + pila.empty());
+        System.out.println("pila.isEmpty()? " + pila.isEmpty());
+        System.out.println("pop : " + pila.pop());
+    }
+}
